3640eb98c21d519d916c3cddb810872ad5d35585
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-04-10 08:51:31 -0700

    tabs section added

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,43 @@
 //
 //  Each tab should look like this:
 //    <div class="tab">topic here</div>
+
+// const getData = axios.get('https://lambda-times-backend.herokuapp.com/topics')
+//object contains data with a topics array inside it
+
+const topics = document.querySelector('.topics')
+
+const tabBuilder = (topic) => {
+     const tab = document.createElement('div')
+     tab.classList.add('tab')
+     tab.textContent = topic
+
+     return tab
+}
+
+
+
+
+
+const getTopics = () => {
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+.then(
+     response => {
+          const newTopics = response.data.topics
+          newTopics.forEach(topic => {
+            const topicTab = tabBuilder(topic)
+               topics.appendChild(topicTab)
+          })
+     }
+
+)
+.catch(
+       //here goes a callback that takes an error
+       //this is the sad path -- the code that runs if things don't work out
+  error => {
+       console.log('error!')
+  }
+)
+}
+
+getTopics()
